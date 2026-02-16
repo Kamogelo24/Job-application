616739a1c1667ef908537cefee5e6e01e0f07bea
Kamogelo24 <kaukamogelo01@gmail.com>
2026-02-16 16:09:54 +0200

    Add files via upload

---
+++ Personal JavaJobapplication/Main.java
@@ -0,0 +1,19 @@
+public abstract class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   Job Application Tracker v1.0   ");
+        System.out.println("=================================");
+
+        // Start the menu system
+        Menu menu = new Menu();
+        menu.start();
+
+        System.out.println("=================================");
+        System.out.println(" Thank you for using the system! ");
+        System.out.println("=================================");
+    }
+
+    
+}
---
+++ Personal JavaJobapplication/Menu.java
@@ -0,0 +1,7 @@
+public class Menu {
+     public void start() {
+        System.out.println("\nMenu system loading...");
+        System.out.println("This will be your main interface.");
+    }
+    
+}
