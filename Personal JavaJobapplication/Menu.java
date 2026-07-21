f3b3ddafece67debab2cafdef4ce7264a6250f93
Kamogelo24 <kaukamogelo01@gmail.com>
2026-07-21 15:27:15 +0200

    Delete Personal JavaJobapplication directory

--- Personal JavaJobapplication/Menu.java
+++
@@ -1,7 +0,0 @@
-public class Menu {
-     public void start() {
-        System.out.println("\nMenu system loading...");
-        System.out.println("This will be your main interface.");
-    }
-    
-}
